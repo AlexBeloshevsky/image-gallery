b04979f7a3fd3a45857f853cc158983fe12eb870
Alex Beloshevsky <Alex.beloshevsky@magisto.com>
2020-12-09 15:29:48 +0200

    hooked up state into the search component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Searchbar} from '../components'
 
 export default function Home (){
+  const [searchterm, setSearchterm] = useState('')
+  const handleChange = (e) => {
+    const {value} = e.target
+    setSearchterm(value)
+}
+
+  const handleSearch = () => {
+    alert(searchterm);
+    setSearchterm('')
+  }
+
   return(
     <Searchbar>
-      <Searchbar.Submit>press me</Searchbar.Submit>
+      <Searchbar.Input value={searchterm} onChange={handleChange}/>
+      <Searchbar.Submit onClick={handleSearch}>press me</Searchbar.Submit>
     </Searchbar>
   )
 }
