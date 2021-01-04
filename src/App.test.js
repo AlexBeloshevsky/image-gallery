c98e8d33796710c8a7567100116cf77001442742
Alex Beloshevsky <Alex.beloshevsky@magisto.com>
2021-01-04 22:18:37 +0200

    added an additional test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders Search Bar', () => {
+  render(<App />);
+  expect(screen.getByText('press me')).toBeInTheDocument();
 });
